f69d3a3c5a977d38a95c95727d38721b98314333
huyokazu <90536886+huyokazu@users.noreply.github.com>
2024-09-10 09:20:53 +0700

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-import streamlit as st 
+import streamlit as st
 
-st.header("hello")
+from streamlit.components.v1 import html
+
+
+
+def open_page(url):
+    open_script= """
+        <script type="text/javascript">
+            window.open('%s', '_blank').focus();
+        </script>
+    """ % (url)
+    html(open_script)
+
+st.button('My instagram', on_click = open_page, args=('https://www.instagram.com/ng_thenkhyy/',))
