a91d27201972c7fbc55d9f30e77224dda31f2992
huyokazu <90536886+huyokazu@users.noreply.github.com>
2024-09-17 18:45:56 +0700

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import streamlit as st
+import time
+import numpy as np
+import pandas as pd
+
 from streamlit.components.v1 import html
+
+st.header("Hello everyone welcome to my website")
+st.subheader("These are my personal pages:")
 
 def open_page(url):
     open_script= """
@@ -9,4 +16,8 @@ def open_page(url):
     """ % (url)
     html(open_script)
 
-st.button('Open link', on_click=open_page, args=('https://www.instagram.com/ng_thenkhyy/',))
+
+
+ig = st.button('My instagram', on_click = open_page, args = ('https://tinyurl.com/4znb2n79',))
+if ig:
+    st.experimental_rerun()
